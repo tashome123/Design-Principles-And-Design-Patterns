c1d72beafab6343dab4808993db125029b8de60e
tashome123 <tashome.makonnen@strathmore.edu>
2025-06-03 13:54:25 +0300

    Single Responsibility Principle violated

---
+++ violated.java
@@ -0,0 +1,16 @@
+package Single.responsibility.principle;
+
+class Report {
+    public void generateReport() {
+        // logic to generate report
+    }
+
+    public void saveToFile(String filename) {
+        // logic to save report to file
+    }
+
+    public void printReport() {
+        // logic to print the report
+    }
+}
+
