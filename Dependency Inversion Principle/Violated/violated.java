11ff6ecbbb9d256201e3fb97b312b14cc4c44224
tashome123 <tashome.makonnen@strathmore.edu>
2025-06-03 14:25:46 +0300

    Add files via upload

---
+++ Dependency Inversion Principle/Violated/violated.java
@@ -0,0 +1,16 @@
+package Open.Open.principle.Dependency.inversion.principle;
+
+class MySQLDatabase {
+    public void connect() {
+        System.out.println("Connected to MySQL");
+    }
+}
+
+class App {
+    private MySQLDatabase db = new MySQLDatabase(); // tightly coupled
+
+    public void start() {
+        db.connect();
+    }
+}
+
